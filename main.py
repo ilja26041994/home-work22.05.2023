a2d972d28f92da9a1385164c9d69fdcc2ddb84a1
ilja26041994 <134002146+ilja26041994@users.noreply.github.com>
2023-05-23 23:02:00 +0300

    6.8 + 6.22 (g<d) 6.23 (а,в)

--- main.py
+++ main.py
@@ -7,16 +7,32 @@
 #     g = cnt ** 2
 #     print('последовательность больше на одно число', g)
 # #6.22
+# z=0
+# c=1
 # x = int(input('input your number'))
-# if x > 0:
-#     while x > 0:
-#         g = x % 10
-#         if g > 0 and g <= 5:
-#             z = g + g
-#             print(z)
-#         if g > 7:
-#             c = g*g
-#             print(c)
-#         x = x // 10
-# else:
-#     print('error')
+# while x > 0:
+#     g = x % 10
+#     if g > 5:
+#         z = z + g
+#     if g > 7:
+#         c = c*g
+#     x = x // 10
+# print(z)
+# print(c)
+# # 6.23 (а,в)
+# cnt=0
+# cnt1=0
+# summ=0
+# natureNumber = int(input('input some nature number\n'))
+# a = int(input('some number 0<=a<=8\n'))
+# if a >= 0 and a <= 8:
+#     while natureNumber > 0:
+#         num = natureNumber % 10
+#         if num == a:
+#             cnt = cnt + 1
+#         if a < num:
+#             summ = summ + num
+#         natureNumber = natureNumber // 10
+# print(summ)
+# print('количество повторившихся а =', cnt)
+
