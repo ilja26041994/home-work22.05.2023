2a0947c9540d8db495453db7eb0b5465c5d4f3be
ilja26041994 <134002146+ilja26041994@users.noreply.github.com>
2023-05-23 21:07:26 +0300

    6.8

---
+++ main.py
@@ -0,0 +1,8 @@
+# # 6.8.
+# x = int(input('some edge number\n'))
+# g=0
+# cnt = 0
+# while g < x:
+#     cnt = cnt + 1
+#     g = cnt ** 2
+#     print('последовательность больше на одно число', g)
