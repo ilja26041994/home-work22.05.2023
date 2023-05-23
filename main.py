fb7cc0cde12fc3780c1532d900bc69f53b7897a1
ilja26041994 <134002146+ilja26041994@users.noreply.github.com>
2023-05-23 21:40:01 +0300

    6.8 + 6.22 (g<d)

--- main.py
+++ main.py
@@ -6,3 +6,17 @@
 #     cnt = cnt + 1
 #     g = cnt ** 2
 #     print('последовательность больше на одно число', g)
+# #6.22
+# x = int(input('input your number'))
+# if x > 0:
+#     while x > 0:
+#         g = x % 10
+#         if g > 0 and g <= 5:
+#             z = g + g
+#             print(z)
+#         if g > 7:
+#             c = g*g
+#             print(c)
+#         x = x // 10
+# else:
+#     print('error')
